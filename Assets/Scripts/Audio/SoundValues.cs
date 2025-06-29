05a3792f0674de69164154942f1299f02cf1ebb7
SebasEsc6 <sebastian.escobar_t@uao.edu.co>
2025-06-28 19:21:10 -0500

    feat : updating sfx for player

---
+++ Assets/Scripts/Audio/SoundValues.cs
@@ -0,0 +1,13 @@
+//Author: Small Hedge Games
+//Updated: 13/06/2024
+
+namespace SmallHedge.SoundManager
+{
+    public enum SoundType
+    {
+        Damage,
+        Attack,
+        Defeat,
+        Jump,
+    }
+}
